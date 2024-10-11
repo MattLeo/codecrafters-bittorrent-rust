9da4cafb8973455d1ff3a7fd467744da4b396cf4
MattLeo <matt.leo@outlook.com>
2024-10-11 17:35:46 +0000

    added mixed encoding

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use hex::decode;
 use serde_json;
 use std::env;
 
@@ -6,19 +7,31 @@ use std::env;
 
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_digit(10) {
+    let ident = encoded_value.chars().next().unwrap();
+
+    if ident.is_digit(10) {
         // Example: "5:hello" -> "hello"
         let colon_index = encoded_value.find(':').unwrap();
         let number_string = &encoded_value[..colon_index];
         let number = number_string.parse::<i64>().unwrap();
         let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
         return serde_json::Value::String(string.to_string());
-    } else if encoded_value.chars().next().unwrap() == 'i' {
+    } else if ident == 'i' {
         let e_index = encoded_value.find('e').unwrap();
         let number_string = &encoded_value[1..e_index];
         let number = number_string.parse::<i64>().unwrap();
         return serde_json::Value::Number(serde_json::Number::from(number));
+    } else if ident == 'l' {
+        let mut list = Vec::new();
+        let mut rest = &encoded_value[1..];
+        while rest.chars().next().unwrap() != 'e' {
+            let item = decode_bencoded_value(rest);
+            let colon_index = rest.find(':').unwrap_or_else(|| rest.find('e').unwrap());
+            rest = &rest[colon_index..];
+            list.push(item);
+
+        }
+        return serde_json::Value::Array(list);
     } else {
         panic!("Unhandled encoded value: {}", encoded_value)
     }
