3ac7af51cd1a00d47e8a653aac0ce92685589d71
MattLeo <matt.leo@outlook.com>
2024-10-18 23:58:24 +0000

    Merging into master

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
+use core::str;
 use std::env;
 use std::path::PathBuf;
 use std::fs::File;
 use std::io::Read;
-use std::str;
 use sha1::{Sha1, Digest};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
@@ -80,7 +80,7 @@ impl TrackerRequest {
             .collect::<String>()
     }
 
-    fn get_response(&self) -> () /*Result<TrackerResponse, Box<Result, std::error::Error>> */{
+    fn get_response(&self) -> Result<Box<[u8]>, Box<dyn std::error::Error>> {
         let request_url = format!(
             "{}?info_hash={}&peer_id={}&port={}&uploaded={}&downloaded={}&left={}&compact={}",
             self.url,
@@ -92,36 +92,127 @@ impl TrackerRequest {
             self.left,
             self.compact,
         );
-        println!("{}", request_url);
-        let response= reqwest::blocking::get(request_url).unwrap();
+        let response= reqwest::blocking::get(request_url)?;
         
         if response.status().is_success() {
-            let bytes = response.bytes().unwrap();
-            match decode_bencoded_value(&bytes) {
-                Ok((decoded, _)) =>
-            }
-
-        } else {
-            println!("HTTP Error");
-        }
+            let bytes: Vec<u8> = response.bytes()?.to_vec();
+            Ok(bytes.into_boxed_slice())
+        } else{
+            Err(format!("HTTP Error: {}", response.status()).into())
+        }        
     }
 }
+
 #[allow(dead_code)]
 #[derive(Debug, Serialize, Deserialize)]
 struct TrackerResponse {
-    complete: i64,
-    incomplete: i64,
-    interval: i64,
+    complete: i32,
+    incomplete: i32,
+    downloaded: i32,
+    interval: i32,
     #[serde(rename = "min interval")]
-    min_interval: i64,
+    min_interval: i32,
     peers: Vec<PeerData>,
+}
+
+impl TrackerResponse {
+    fn new(byte_array: &[u8]) -> Result<TrackerResponse, Box<dyn std::error::Error>> {
+        let mut i = 0;
+        let mut complete = 0;
+        let mut downloaded = 0;
+        let mut incomplete = 0;
+        let mut interval = 0;
+        let mut min_interval = 0;
+        let mut peers: Vec<PeerData> = Vec::new();
+
+        if byte_array[i] != b'd' {
+            return Err("Tracker Response is not a dictionary".into());
+        }
+        println!("Received data: {:?}", String::from_utf8_lossy(byte_array));
+        i += 1;
+        while i < byte_array.len() && byte_array[i] != b'e' {
+            println!("Parsing at index: {}", i);
+            let colon_pos = byte_array[i..].iter().position(|&b| b == b':')
+                .ok_or("WHAT.")?;
+            let key_len: usize = std::str::from_utf8(&byte_array[i..i + colon_pos])?
+                .parse()?;
+            i += colon_pos + 1;
+
+            if i + key_len > byte_array.len() {
+                return Err("Key length exceeds remaining data".into());
+            }
+
+            let key = std::str::from_utf8(&byte_array[i..i+key_len])?;
+            i += key_len;
+
+            match key {
+                "complete" => {
+                    (complete, i) = TrackerResponse::extract_int(byte_array, i)?;
+                },
+                "downloaded" => {
+                    (downloaded, i) = TrackerResponse::extract_int(byte_array, i)?;
+                },
+                "incomplete" => {
+                    (incomplete, i) = TrackerResponse::extract_int(byte_array, i)?;
+                },
+                "interval" => {
+                    (interval, i) = TrackerResponse::extract_int(byte_array, i)?;
+                },
+                "min interval" => {
+                    (min_interval, i) = TrackerResponse::extract_int(byte_array, i)?;
+                },
+                "peers" => {
+                    let colon_pos = byte_array[i..].iter().position(|&b| b == b':')
+                        .ok_or("Invalud 'peers' encoding")?;
+                    let peers_len: usize = std::str::from_utf8(&byte_array[i..i + colon_pos])?
+                        .parse()?;
+                    i += colon_pos + 1;
+                    let peers_data = &byte_array[i..i + peers_len];
+                    for chunk in peers_data.chunks(6) {
+                        if chunk.len() == 6 {
+                            let ip = format!("{}.{}.{}.{}", chunk[0], chunk[1], chunk[2], chunk[3]);
+                            let port = u16::from_be_bytes([chunk[4], chunk[5]]);
+                            let peer= PeerData {ip, port};
+                            peers.push(peer);
+                        } else {
+                            return Err("Malformed peer data received".into());
+                        }
+                    }
+                    break;
+                },
+                _=> {
+                    return Err("Invalid key received in response".into());
+                }
+            }
+        }
+        Ok ((TrackerResponse {
+            complete,
+            downloaded,
+            incomplete,
+            interval,
+            min_interval,
+            peers
+        }))
+    }
+
+    fn extract_int(byte_array: &[u8], mut i: usize) -> Result<(i32, usize), Box<dyn std::error::Error>> {
+        if byte_array[i] != b'i' {
+            return Err("Key's value is not an integer".into());
+        }
+        i += 1;
+        let end = byte_array[i..].iter().position(|&b| b == b'e')
+            .ok_or("Invalid integer encoding")?;
+        let return_value = std::str::from_utf8(&byte_array[i..i+end])?.parse::<i32>()?;
+        i += end + 1;
+        Ok((return_value, i))
+    }
 }
 
 #[allow(dead_code)]
 #[derive(Debug, Serialize, Deserialize)]
 struct PeerData {
     ip: String,
-    port: i16,
+    port: u16,
 }
 
 fn decode_bencoded_value(encoded_value: &[u8]) -> Result<(Value, usize), Box<dyn std::error::Error>> {
@@ -202,7 +293,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             let torrent = Torrent::new(PathBuf::from(argument))?;
             let info_hash = torrent.info_hash()?;
             let request = TrackerRequest::new(torrent.announce, info_hash, torrent.info.length);
-            request.get_response();
+            let response = request.get_response().unwrap();
+            let tracker_info: TrackerResponse = TrackerResponse::new(&*response)?;
+
+            for peer in tracker_info.peers {
+                println!("{}:{}", peer.ip, peer.port);
+            }
         }
 
         _ => eprintln!("Unknown command: {}", command),
