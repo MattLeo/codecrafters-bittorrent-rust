61882e916e29b4b6fcb771548cde125356d13a47
MattLeo <matt.leo@outlook.com>
2024-10-22 02:34:53 +0000

    pushing again

--- src/main.rs
+++ src/main.rs
@@ -392,7 +392,7 @@ fn recieve_response(stream: &mut TcpStream) -> Result<Option<(u32, u32, Vec<u8>)
     }
 }
 
-fn join_blocks( blocks: Vec<(u32, Vec<u8>)>, peice_length: u32,) -> Vec<u8> {
+fn join_blocks(blocks: Vec<(u32, Vec<u8>)>, peice_length: u32) -> Vec<u8> {
     let mut piece_buffer = vec![0u8; peice_length as usize];
     for (block_offset, block_data) in blocks {
         let offset = block_offset as usize;
@@ -429,6 +429,16 @@ fn read_exact_with_retry(stream: &mut TcpStream, buf: &mut [u8]) -> std::io::Res
     Ok(())
 }
 
+fn join_pieces(pieces: Vec<(u32, Vec<u8>)>, file_length: u32) -> Vec<u8> {
+    let mut file_buffer = vec![0u8; file_length as usize];
+    for (piece_offset, piece_data) in pieces {
+        let start = piece_offset as usize;
+        let end = start + piece_data.len() as usize;
+        file_buffer[start..end].copy_from_slice(&piece_data);
+    }
+    file_buffer
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
@@ -439,7 +449,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let command = &args[1];
     let argument = &args[2];
 
-    match command.as_str() {
+    let args: Vec<String> = env::args().collect();
+    println!("Number of args: {}", args.len());
+    for (i, arg) in args.iter().enumerate() {
+        println!("Arg {}: '{}' (bytes: {:?})", i, arg, arg.as_bytes());
+    }
+
+    let command = args[1].trim();
+    println!("Command after trim: '{}' (bytes: {:?})", command, command.as_bytes());
+
+    match command.trim().to_lowercase().as_str() {
         "decode" => {
             let decoded_value = (decode_bencoded_value(argument.as_bytes())?).0;
             println!("{}", serde_json::to_string(&decoded_value)?);
@@ -547,6 +566,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                     None => {}
                 }
             }
+            
 
             let full_piece = join_blocks(blocks, piece_length as u32);
             let mut hasher = Sha1::new();
@@ -558,6 +578,88 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             } else {
                 eprintln!("Recieved piece does not match hash data");
             }
+        }
+
+        "download" => {
+            let output_path = Path::new(&args[3]);
+            if let Some(parent_dir) = output_path.parent() {
+                fs::create_dir_all(parent_dir)?
+            }
+            let mut file = File::create(output_path)?;
+            let max_requests = 5;
+
+            let torrent = Torrent::new(PathBuf::from(&args[4]))?;
+            let total_file_length = torrent.info.length.clone() as u32;
+            let total_pieces = (total_file_length as f64 / torrent.info.piece_length.clone() as f64).ceil() as u32;
+            
+            let torrent_clone = torrent.clone();
+            let info_hash = torrent.info_hash()?;
+            let info_hash_clone = info_hash.clone();
+            let client_id = "TestRTAAA11234567899".to_string();
+            let request = TrackerRequest::new(torrent.announce, info_hash, torrent.info.length);
+            let response = request.get_response().unwrap();
+            let tracker_info: TrackerResponse = TrackerResponse::new(&*response)?;
+            let handshake = Handshake::new(info_hash_clone, client_id);
+            let mut handshake_response = [0u8; 68];
+            let peer = &tracker_info.peers[0];
+            
+            let mut stream = TcpStream::connect(format!("{}:{}",peer.ip, peer.port ))?;
+            stream.write_all(&handshake.get())?;
+            stream.read_exact(&mut handshake_response)?;
+            let _bitfield = recieve_response(&mut stream)?;
+            let mut pieces: Vec<(u32,Vec<u8>)> = Vec::new();
+            send_message("interested", &mut stream, &0, 0, 0)?;
+            let _unchoke = recieve_response(&mut stream)?;
+
+            for piece_index in 0..total_pieces {
+                let mut block_offset: u32 = 0;
+                let mut pending_requests: usize = 0;
+                let mut blocks: Vec<(u32,Vec<u8>)> = Vec::new();
+                let mut block_size: u32 = 16 * 1024;
+                let mut piece_length = torrent.info.piece_length.clone() as u32;
+                let mut block_index = 0;
+                if piece_index == total_pieces - 1 {
+                    let mut last_piece_length = total_file_length % piece_length;
+                    if last_piece_length == 0 {
+                        last_piece_length = piece_length;
+                    }
+                    piece_length = last_piece_length;
+                }
+                
+                while block_offset < piece_length as u32 {
+                    if block_offset < piece_length as u32 && pending_requests < max_requests {
+                        println!("Sending request");
+                        send_message("request", &mut stream, &piece_index, block_offset, block_size)?;
+                        println!("{} offset of {} total", block_offset, piece_length);
+                        block_offset += block_size;
+                        pending_requests += 1;
+                        block_index += 1;
+                        block_size = calc_block_size(piece_length, block_size, block_index);
+                    }
+    
+                    match recieve_response(&mut stream)? {
+                        Some((_received_piece_index, received_block_offset, block_data)) => {
+                            println!("Received block");
+                            blocks.push((received_block_offset, block_data));
+                            pending_requests -= 1;
+                        }
+                        None => {}
+                    }
+                }
+
+                let full_piece = join_blocks(blocks, piece_length as u32);
+                let mut hasher = Sha1::new();
+                hasher.update(&full_piece);
+                let full_piece_hash = hex::encode(hasher.finalize());
+    
+                if torrent_clone.validate_piece(&piece_index, full_piece_hash) {
+                    pieces.push((piece_length, full_piece));
+                } else {
+                    eprintln!("Recieved piece does not match hash data");
+                }
+            }
+            let full_file = join_pieces(pieces, total_file_length);
+            file.write_all(&full_file)?;
         } 
 
         _ => eprintln!("Unknown command: {}", command),
